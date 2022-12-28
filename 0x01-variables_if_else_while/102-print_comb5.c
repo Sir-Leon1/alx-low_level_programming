feddb100ff7f7c54f6c076ce66e495ccbc189b1d
Leon Gunnah <114517374+Sir-Leon1@users.noreply.github.com>
2022-12-28 20:55:14 +0000

    More loops

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+	int n, n2;
+	for (n = 0; n <= 98; n++)
+	{
+		for  (n2 = (n + 1); n2 <= 99; n2++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
+
+			if (n == 98 && n2 == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+
+
+		}
+	}
+	putchar('\n');
+	return (0);
+}
