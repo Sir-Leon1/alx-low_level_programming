047dd386798b56004ef40949e0bd3bf0db237e68
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2023-12-23 16:51:56 +0300

    To main.h...

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,8 +4,7 @@
  *
  */
 
-#include "holberton.h"
-
+#include "main.h"
 /**
  * main - Prints "Holberton" followed by a new line.
  *
@@ -13,16 +12,14 @@
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
 	return (0);
 }
