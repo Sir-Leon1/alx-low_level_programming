a1541ea3cd8061657f4171ec0639d3e1db92db6f
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-01-04 17:27:29 +0300

    Main

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * Desc: Header file containing prototypes for all functions used in the 
+ * 0x04-more_functions_nested_loops directory.
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+
+#endif
