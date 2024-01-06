5013854271ea218bfe129c35e0e45ab2b66a160e
Sir-Leon1 <eb3.61594.22@student.chuka.ac.ke>
2024-01-06 11:53:10 +0300

    Task 1

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc : Header file cont prototypes for all functios
+ */
+
+#include <stdlib.h>
+
+int _putchar(char c);
+void reset_to_98(int n);
+
+#endif
